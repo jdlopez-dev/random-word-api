399bc3f4abfd3f17df724720a60f8aeb353c8e78
Juan <72524993+jdlopez-dev@users.noreply.github.com>
2021-11-12 10:23:36 +0100

    feat: :tada: Initial commit

---
+++ src/test/java/com/randomword/api/RandomWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomword.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
